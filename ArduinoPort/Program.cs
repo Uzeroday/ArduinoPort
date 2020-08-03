99fffc76fb12089ab6de73ad4db3726031b86fa0
HumanError <38556412+nonthawat776@users.noreply.github.com>
2020-08-04 00:59:23 +0700

    Add project files.

---
+++ ArduinoPort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoPort
+{
+    class Program
+    {
+       
+ private static void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.Write("Value : "  + indata);
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            String portNumber = "COM3";
+            SerialPort port = new SerialPort(portNumber, 9600, Parity.None, 8, StopBits.One);
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            Console.WriteLine( "ArdionpPort ep.1");
+            Console.WriteLine();
+
+
+
+            try
+            {
+                port.Open();
+                Console.WriteLine( "Port : " + portNumber + " Sucsess");
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                Console.ReadKey();
+                return;
+            }
+            for (int i = 0; i <= 100; i++)
+            {
+                String key = Console.ReadLine();
+                if (key.Equals("end"))
+                {
+                    port.Close();
+                    i = 101;
+                }
+                else
+                {
+                    port.Write(key);
+                }
+            }
+        }
+    }
+}
+
